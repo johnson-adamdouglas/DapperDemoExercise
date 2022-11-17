34e72f2c8bdfd066c5f23c6582a4ceca079aea21
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-11-17 12:19:26 -0500

    departments

---
+++ DapperDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DapperDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperDepartmentRepository(conn);
+
+            var depts = repo.GetAllDepartments();
+
+            foreach (var dept in depts)
+            {
+                Console.WriteLine(dept.Name);
+            }
+            Console.WriteLine("Enter a new department name");
+            var newDept = Console.ReadLine();
+            repo.InsertDepartment(newDept);
+            Console.WriteLine();
+            depts = repo.GetAllDepartments();
+            foreach (var dept in depts)
+            {
+                Console.WriteLine(dept.Name);
+            }
+
+        }
+    }
+}
